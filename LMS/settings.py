f522793f332f6852e64ae8260a621f15259dac60
Nitin658 <nipracademy@gmail.com>
2024-08-24 14:36:37 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -151,6 +151,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media' )
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10737418240  # 10 GB (adjust as necessary)
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10737418240  # 10 GB (adjust as necessary)
 
+#HTTPS settings (make true to all of them after ssl certification complited)
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+#HSTS settings
+SECURE_HSTS_SECONDS = 31536000 # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
